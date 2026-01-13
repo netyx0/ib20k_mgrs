4e3c5706efd462372bbef1e5f9284aa935fb6ca5
Netyx <netyx@tfwno.gf>
2026-01-13 22:28:54 +0800

    add previewing selected export area for 1:20000 maps

--- scripts/generate.js
+++ scripts/generate.js
@@ -3,11 +3,15 @@
  * https://openlayers.org/en/latest/examples/export-pdf.html
 */
 
+var export_area_polygon_layer = null;
+
 function onclick_export_1to20k()
 {
 	var div;
 	var control_panel = document.getElementById("controls");
 	var el;
+
+	map_selection_enabled = true;
 
 	if (document.getElementById("controls_menu_1to20k")) {
 		return;
@@ -24,8 +28,9 @@ function onclick_export_1to20k()
 	div.appendChild(el);
 
 	el = document.createElement("button");
+	el.id = "export_1to20k_export_btn";
 	el.innerText = "Export selection";
-	el.onclick = onclick_export_1to20k_export;
+	el.onclick = function(){alert("No area selected!");};
 	div.appendChild(el);
 
 	el = document.createElement("button");
@@ -40,7 +45,9 @@ function onclick_export_1to20k()
 
 	control_panel.appendChild(div);
 
-	/* */
+	map.on("click", export_map_clickfunc);
+
+	document.body.style.cursor = "crosshair";
 }
 
 function onclick_export_cur()
@@ -48,27 +55,33 @@ function onclick_export_cur()
 	export_cur_view_curres();
 }
 
-function onclick_export_1to20k_export()
-{
-	alert("not yet implemented :3");
-}
-
 function onclick_export_1to20k_cancel()
 {
+	if (export_area_polygon_layer) {
+		map.removeLayer(export_area_polygon_layer);
+		export_area_polygon_layer = null;
+	}
 	document.getElementById("controls_menu_1to20k").remove();
+	map.removeEventListener("click", export_map_clickfunc);
+	document.body.style.cursor = "auto";
 }
 
 function export_cur_view_1to20k()
 {
-	export_cur_view((29.7*200)/(297*150/25.4));
+	export_cur_view_1to20k_offset(null);
+}
+
+function export_cur_view_1to20k_offset(end_location)
+{
+	export_cur_view((29.7*200)/(297*150/25.4), end_location);
 }
 
 function export_cur_view_curres()
 {
-	export_cur_view(map.getView().getResolution());
+	export_cur_view(map.getView().getResolution(), null);
 }
 
-function export_cur_view(new_resolution)
+function export_cur_view(new_resolution, end_location)
 {
 	const resolution = 150;
 	const dim = [297, 210];
@@ -76,6 +89,15 @@ function export_cur_view(new_resolution)
 	const height = Math.round((210 * resolution) / 25.4);
 	const size = map.getSize();
 	const orig_resolution = map.getView().getResolution();
+	const orig_cursor = document.body.style.cursor;
+
+	var coverdiv = document.createElement("div");
+	coverdiv.id = "coverdiv";
+	coverdiv.innerText = "Loading...";
+	document.body.appendChild(coverdiv);
+	if (export_area_polygon_layer) {
+		map.removeLayer(export_area_polygon_layer);
+	}
 
 	document.body.style.cursor = "wait";
 	map.once("rendercomplete", function() {
@@ -111,8 +133,43 @@ function export_cur_view(new_resolution)
 		pdf.save("map.pdf");
 		map.setSize(size);
 		map.getView().setResolution(orig_resolution);
-		document.body.style.cursor = "auto";
+		if (end_location) {
+			map.getView().setCenter(end_location);
+		}
+		document.body.style.cursor = orig_cursor;
+		coverdiv.remove();
 	});
 	map.setSize([width, height]);
 	map.getView().setResolution(new_resolution);
+}
+
+function export_map_clickfunc(e)
+{
+	var x = e.coordinate[0];
+	var y = e.coordinate[1];
+	if (!map_selection_enabled) {
+		return;
+	}
+	if (export_area_polygon_layer) {
+		map.removeLayer(export_area_polygon_layer);
+		export_area_polygon_layer = null;
+	}
+	export_area_polygon_layer = new ol.layer.Vector({
+		source: new ol.source.Vector({
+			features: [new ol.Feature({geometry: new ol.geom.Polygon([[
+				[x, y],
+				[x + 29.7*200, y],
+				[x + 29.7*200, y - 21.0*200],
+				[x, y - 21.0*200],
+				[x, y]
+			]])})]
+		})
+	});
+	map.addLayer(export_area_polygon_layer);
+	document.getElementById("export_1to20k_export_btn").onclick = function() {
+		document.getElementById("export_1to20k_export_btn").onclick = function(){alert("No area selected!");};
+		var orig_centre = map.getView().getCenter();
+		map.getView().setCenter([x + 29.7/2*200, y - 21.0/2*200]);
+		export_cur_view_1to20k_offset(orig_centre);
+	}
 }
