d15365589f77cf2e45864c05b9d97d0540c77d19
Netyx <netyx@tfwno.gf>
2026-01-13 20:21:16 +0800

    add export at current scale

--- scripts/generate.js
+++ scripts/generate.js
@@ -5,16 +5,80 @@
 
 function onclick_export_1to20k()
 {
+	var div;
+	var control_panel = document.getElementById("controls");
+	var el;
+
+	if (document.getElementById("controls_menu_1to20k")) {
+		return;
+	}
+
+	div = document.createElement("div");
+	div.id = "controls_menu_1to20k";
+	div.classList.add("controldiv")
+
+	div.appendChild(document.createElement("hr"));
+
+	el = document.createElement("p");
+	el.innerText = "Export (1:20k)";
+	div.appendChild(el);
+
+	el = document.createElement("button");
+	el.innerText = "Export selection";
+	el.onclick = onclick_export_1to20k_export;
+	div.appendChild(el);
+
+	el = document.createElement("button");
+	el.innerText = "Export current view";
+	el.onclick = export_cur_view_1to20k;
+	div.appendChild(el);
+
+	el = document.createElement("button");
+	el.innerText = "Cancel";
+	el.onclick = onclick_export_1to20k_cancel;
+	div.appendChild(el);
+
+	control_panel.appendChild(div);
+
+	/* */
+}
+
+function onclick_export_cur()
+{
+	export_cur_view_curres();
+}
+
+function onclick_export_1to20k_export()
+{
+	alert("not yet implemented :3");
+}
+
+function onclick_export_1to20k_cancel()
+{
+	document.getElementById("controls_menu_1to20k").remove();
+}
+
+function export_cur_view_1to20k()
+{
+	export_cur_view((29.7*200)/(297*150/25.4));
+}
+
+function export_cur_view_curres()
+{
+	export_cur_view(map.getView().getResolution());
+}
+
+function export_cur_view(new_resolution)
+{
 	const resolution = 150;
 	const dim = [297, 210];
 	const width = Math.round((297 * resolution) / 25.4);
 	const height = Math.round((210 * resolution) / 25.4);
 	const size = map.getSize();
 	const orig_resolution = map.getView().getResolution();
-	const new_resolution = (29.7*200)/(297*150/25.4);
 
 	document.body.style.cursor = "wait";
-	map.once("rendercomplete", function () {
+	map.once("rendercomplete", function() {
 		const mapCanvas = document.createElement("canvas");
 		mapCanvas.width = width;
 		mapCanvas.height = height;
